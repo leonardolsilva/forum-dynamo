9b8d8f852f4bcd8221fda57eb3fea23c2fc5defe
Leonardo Lopes <leo_lopes09@hotmail.com>
2021-05-09 18:34:29 -0300

    First commit

---
+++ src/main/java/br/com/lelecoder/forumdynamo/ForumDynamoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lelecoder.forumdynamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumDynamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumDynamoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lelecoder/forumdynamo/ForumDynamoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lelecoder.forumdynamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumDynamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
